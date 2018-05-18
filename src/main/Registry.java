3e725706d727e70099314351b69053b173d2d498
Jaskrit Singh <jaskrits.singh@gmail.com>
2018-05-18 15:59:27 -0600

    New Spells and Weapons

--- src/main/Registry.java
+++ src/main/Registry.java
@@ -8,6 +8,10 @@ import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
+import entity.Perk;
+import entity.Spell;
+import entity.Unit;
+import entity.UnitClass;
 import windows.StartMenuWindow;
 import windows.world.Overlay;
 import windows.world.World;
@@ -113,4 +117,16 @@ public class Registry {
 
 		return img;
 	}
+	public void registerSpells() {
+		Spell smashAttack = new Spell("Smash Attack", "earth",3, 1, 1);
+		Spell rockWall = new Spell("Rock Wall", "earth", 2, 0, 2, 0, 100, 2, 1, "rock wall");
+		Spell sink = new Spell("Sink", "earth", 5, 0, 3, "bury", 0, true, 2, 0);
+		Spell sandstorm = new Spell("Sandstorm", "earth", 4, .5, 7, .5, 3, 5, 5);
+		Spell landslide = new Spell("Landslide", "earth", 4, 1.5, 4, 0, 1, 1, 3);
+		
+		
+		Spell bubble = new Spell("Bubble","water",5,.75,1);
+		Spell steamBlast = new Spell("Steam Blast","water",3,1,1);
+	}
+	
 }
--- src/main/Spells.java
+++ src/main/Spells.java
@@ -3,12 +3,6 @@ package main;
 import entity.Spell;
 
 public class Spells {
-	Spell smashAttack = new Spell("Smash Attack", "earth",3, 1, 1);
-	Spell rockWall = new Spell("Rock Wall", "earth", 2, 0, 2, 0, 100, 2, 1, "rock wall");
-	Spell sink = new Spell("Sink", "earth", 5, 0, 3, "bury", 0, true, 2, 0);
-	Spell sandstorm = new Spell("Sandstorm", "earth", 4, .5, 7, .5, 3, 5, 5);
-	Spell landslide = new Spell("Landslide", "earth", 4, 1.5, 4, 0, 1, 1, 3);
-	
 	
 
 
